40257a436e6c3850df83a94f05c34e4ce8ffc4b7
Ivan Shnurchenko <stripedtigra@gmail.com>
2016-01-02 22:58:16 +0300

    Added human-readable for notes

--- blog/blog_app/urls.py
+++ blog/blog_app/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     # ex: /
     url(r'^$', views.NotesIndexView.as_view(), name='notes_index'),
     # ex: /note/3
-    url(r'^note/(?P<pk>[0-9]+)/$', views.NoteDetailView.as_view(),
-        name='note_details'),
+    url(r'^note/(?P<url>[\w-]+)', views.note_detail_view, name='note_details'),
 ]

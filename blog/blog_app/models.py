49a620ee7f8bf0aede2c53c766791fb075323ae1
Ivan Shnurchenko <stripedtigra@gmail.com>
2016-01-03 17:45:18 +0300

    Added markdown for note.body (migration)

--- blog/blog_app/models.py
+++ blog/blog_app/models.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 
+from markdown import markdown
 from taggit.managers import TaggableManager
 from django.db import models
 from django.utils import timezone
@@ -9,6 +10,7 @@ from slugify import slugify
 class Note(models.Model):
     title = models.CharField(max_length=255, blank=True)
     body = models.TextField(max_length=4000, blank=True)
+    body_html = models.TextField(max_length=4000, blank=True, editable = False)
     pub_date = models.DateTimeField('date published', default=timezone.now)
     tags = TaggableManager(blank=True)
     url = models.CharField(max_length=2000, default=None, blank=True)
@@ -16,6 +18,7 @@ class Note(models.Model):
     def save(self, *args, **kwargs):
         if not self.id:
             self.url = slugify(self.title)
+        self.body_html = markdown(self.body)
         super(Note, self).save(*args, **kwargs)
 
     def __str__(self):

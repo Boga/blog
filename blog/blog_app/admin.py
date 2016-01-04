0055461020c5e99745181436e76bb29cc133d7f8
Ivan Shnurchenko <stripedtigra@gmail.com>
2016-01-04 15:39:27 +0300

    Added files attaching to note

--- blog/blog_app/admin.py
+++ blog/blog_app/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 
-from .models import Note
+from .models import Note, AttachedFile
+
+
+class AttachedFileInlineAdmin(admin.TabularInline):
+    model = AttachedFile
 
 
 class NoteAdmin(admin.ModelAdmin):
     list_display = ('title', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['title', 'body']
+    inlines = [AttachedFileInlineAdmin]
+
 
 admin.site.register(Note, NoteAdmin)
+# admin.site.register(Image)
--- blog/blog_app/models.py
+++ blog/blog_app/models.py
@@ -9,12 +9,11 @@ from slugify import slugify
 
 class Note(models.Model):
     title = models.CharField(max_length=255, blank=True)
-    body = models.TextField(max_length=4000, blank=True)
+    body = models.TextField(max_length=4000, blank=True, db_index=True)
     body_html = models.TextField(max_length=4000, blank=True, editable = False)
     pub_date = models.DateTimeField('date published', default=timezone.now)
     tags = TaggableManager(blank=True)
     url = models.CharField(max_length=2000, default=None, blank=True)
-    image = models.ImageField(blank=True)
 
     def save(self, *args, **kwargs):
         if not self.id:
@@ -26,7 +25,13 @@ class Note(models.Model):
         return '%r %s' % (self.id, self.title)
 
     def __unicode__(self):
-        return '%r %s' % (self.id, self.title)
+        return self.__str__()
 
     class Meta:
         ordering = ('-pub_date',)
+
+
+class AttachedFile(models.Model):
+    file = models.FileField(upload_to='%Y/%m/%d/')
+    note = models.ForeignKey(Note, on_delete=models.CASCADE,
+                             related_name='attaches')

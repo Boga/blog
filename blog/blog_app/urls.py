820dcb6647b30dabfd4e18c411384f29d81fbb30
Ivan Shnurchenko <stripedtigra@gmail.com>
2016-01-06 22:43:40 +0300

    Added stub flatpages.

--- blog/blog_app/urls.py
+++ blog/blog_app/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 
 from . import views
 
@@ -8,4 +9,10 @@ urlpatterns = [
     url(r'^$', views.NotesIndexView.as_view(), name='notes_index'),
     # ex: /note/3
     url(r'^note/(?P<url>[\w-]+)', views.note_detail_view, name='note_details'),
+    # /about
+    url(r'^about/', TemplateView.as_view(template_name="blog_app/about.html")),
+    url(r'^resume/',
+        TemplateView.as_view(template_name="blog_app/resume.html")),
+    url(r'^projects/', TemplateView.as_view(
+            template_name="blog_app/projects.html")),
 ]
